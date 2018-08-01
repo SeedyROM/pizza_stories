b77d55cc65105db98e754fe0ded5beff1882aa62
Zack Kollar <rallokkcaz@gmail.com>
2018-07-31 21:20:33 -0700

    Added some features

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('<int:id>', views.detail, name='detail'),
+    path('', views.all, name='all'),
+]
